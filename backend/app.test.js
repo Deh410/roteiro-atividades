c490b68be386b34c9f70bdbad83f6c0741019d03
deh410 <aearahawa@gmail.com>
2020-10-15 19:46:37 -0300

    set better tests

--- backend/app.test.js
+++ backend/app.test.js
@@ -38,5 +38,30 @@ describe('app', function() {
     expect(testApp.messages.length).to.equal(0)
   })
 
+  it("id's are always unique", function() {
+    testApp.post('1')
+    testApp.post('2')
+    testApp.delete(1)
+    testApp.post('3')
+    expect(testApp.messages[1].id).to.equal(3)
+  })
+
+  it("app deletes correctly", function() {
+    testApp.post('1')
+    testApp.post('2')
+    testApp.post('3')
+    testApp.delete(0)
+    testApp.delete(2)
+    expect(testApp.get(1).id).to.equal(1)
+  })
+
+  it("app updates correctly", function() {
+    testApp.post('1')
+    testApp.post('2')
+    testApp.delete(1)
+    testApp.update(2, 'update')
+  expect(testApp.get(2).content).to.equal('update')
+  })
+
 })
 
