de59ee8961decf5466b28d9d9825a435e2ac8310
deh410 <aearahawa@gmail.com>
2020-10-16 20:29:41 -0300

    add cors to app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import routes from './lib/routes.js'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(express.urlencoded({
   extended: true
 }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use(routes)
 
