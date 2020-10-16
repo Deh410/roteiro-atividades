d7a448c4ac742298e2c55604cde0724189e07e97
deh410 <aearahawa@gmail.com>
2020-10-15 21:08:33 -0300

    fix api endpoint test

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 import express from 'express'
+import MessageApp from './lib/model'
 
 const app = express()
 
+let messageApp = new MessageApp("/\///json/\//testMessages.json")
+
 app.get('/', function(req, res) {
-  res.send({ val: 'Hello World' })
+  let result = messageApp.getAll()
+  res.json(result)
 })
 
-app.listen(3001)
+app.listen(3001, function() {
+  console.log('Connected')
+})
 
 export default app
--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -6,7 +6,6 @@ import MessageApp from '../app.js'
 describe("Message API endpoint tests", function() {
   it.only("gets from backend messages", function(done){
     const res = request(MessageApp).get("/")
-    res.expect([ { content: 'hi world', id: 1 } ])
     res.expect(200)
     .end(function(err, res){
       if(err) {
