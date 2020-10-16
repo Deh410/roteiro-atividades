8cbc76e29441cb427ee41c50d980490a51b69da2
deh410 <aearahawa@gmail.com>
2020-10-16 20:48:31 -0300

    add first structural tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import MessageApp from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { mount } from 'enzyme'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('App', () => {
+  const component = mount(<MessageApp/>)
+  it('renders without crashing', () => {
+    expect(component).toMatchSnapshot()
+  })
+
+  it('has textbox', () => {
+    expect(component.exists('textarea#message_box')).toBe(true)
+  })
+
+  it('has submit button', () => {
+    expect(component.exists('button#submit')).toBe(true)
+  })
+
+  it('has message list', () => {
+    expect(component.exists('ul#message_list')).toBe(true)
+  })
+})
