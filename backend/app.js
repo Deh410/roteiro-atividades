6985eea2f55fc5098eae05ed67d4d3dfebec0546
deh410 <aearahawa@gmail.com>
2020-10-16 09:54:54 -0300

    move req from app to routes

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,6 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', function(req, res) {
-  let result = messageApp.getAll()
-  res.json(result)
-})
-
 app.listen(3001, function() {
   console.log('Connected')
 })
