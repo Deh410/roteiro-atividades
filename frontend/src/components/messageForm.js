185260a86c90846478dc217858cfce43daf8a46a
deh410 <aearahawa@gmail.com>
2020-10-17 11:32:42 -0300

    handle submit to send and clear data

--- frontend/src/components/messageForm.js
+++ frontend/src/components/messageForm.js
@@ -1,10 +1,36 @@
 import React from 'react'
 
 class MessageForm extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentMessage: ''
+    }
+  }
+
+  changeMessageValue(change) {
+    this.setState({
+      currentMessage: change
+    })
+  }
+
+  processSubmit(e){
+    e.preventDefault()
+    this.changeMessageValue('')
+  }
+
   render() {
     return(
-      <form id="message_form">
-      <textarea id='message_box'></textarea>
+      <form 
+        id="message_form"
+        ref='formRef'
+        onSubmit={(e) => this.processSubmit(e)}  
+      >
+      <textarea 
+        id='message_box'
+        onChange={(e) => this.changeMessageValue(e.target.value)}
+        value={ this.state.currentMessage }
+      ></textarea>
       <br/>
       <button 
         id="submit"
